cfa9a651da1a2a5199997a35db6702567c44ca9c
Joseph Dickan <jdickan@ucla.edu>
2020-11-20 14:01:03 +0200

    setapp install to bin

--- bin.homelink/setapp-install.sh
+++ bin.homelink/setapp-install.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ## Outputs installed Setapp app list
-## Called by Hazel ("Setapp Installed") whenever a new app is added
+## Hazel runs as an embedded script ("Setapp Installed") whenever a new app is added
 ls /Applications/Setapp | egrep '\.app$' > ~/.dotfiles/setapp-install.txt
