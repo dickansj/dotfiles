5c746341c92772df9a92f573f0d3b38ecb209e2b
Joseph Dickan <jdickan@ucla.edu>
2020-11-21 19:19:16 +0200

    Add aliases

--- zsh.d.symlink/aliases.zsh
+++ zsh.d.symlink/aliases.zsh
@@ -5,11 +5,13 @@ if $(gls &>/dev/null); then
   alias ll="gls -lh --color"
   alias la="gls -FA --color"
   alias lla="gls -lhA --color"
+  alias lsp="gls -lhA --color"
 else
   alias ls="ls -F --color"
   alias ll="ls -lh --color"
   alias la="ls -FA --color"
   alias lla="ls -lhA --color"
+  alias lsp="ls -lhA --color"
 fi
 
 alias mkdir="mkdir -p"
@@ -21,3 +23,11 @@ if [[ $OSTYPE == darwin* ]]; then
   fi
 fi
 
+ alias ..='cd ../'             # Go up 1 dir level
+ alias ...='cd ../../'         # Go up 2 dir levels
+ alias .3='cd ../../../'       # Go up 3 dir levels
+
+ # Homebrew shortcut alias
+ # Consider doing environment upgrade
+ # https://gist.github.com/indiesquidge/ec010eca3ffa254788c2
+ alias brewup='brew update; brew upgrade; brew cleanup; brew doctor'
