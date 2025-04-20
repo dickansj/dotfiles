3d973d98e37d4010792b21732e48df22ce250dac
Joseph Dickan <jdickan@ucla.edu>
2025-04-19 21:58:13 -0400

    Update bootstrap to fork tarball

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,8 +8,8 @@ if [[ -d $scriptPath/.dotfiles ]]; then
   exit 1
 fi
 
-curl -L https://api.github.com/repos/sjml/dotfiles/tarball > $scriptPath/dotfiles.tar.gz
-pathName=$(tar -ztvf dotfiles.tar.gz | head -1 | awk 'match($0,/sjml-dotfiles-([a-f0-9]*)/) {print substr($0,RSTART,RLENGTH)}')
+curl -L https://api.github.com/repos/dickansj/dotfiles/tarball > $scriptPath/dotfiles.tar.gz
+pathName=$(tar -ztvf dotfiles.tar.gz | head -1 | awk 'match($0,/dickansj-dotfiles-([a-f0-9]*)/) {print substr($0,RSTART,RLENGTH)}')
 tar -xzf dotfiles.tar.gz
 rm $scriptPath/dotfiles.tar.gz
 mv $scriptPath/$pathName $scriptPath/.dotfiles
