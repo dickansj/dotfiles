b9ecc05d133726680ede084f2278838c10ceb91b
Joseph Dickan <jdickan@ucla.edu>
2026-07-09 16:34:59 -0700

    Swap deprecated egrep for grep -E in setapp-install.sh

--- utility/setapp-install.sh
+++ utility/setapp-install.sh
@@ -2,4 +2,4 @@
 
 ## Outputs installed Setapp app list
 ## Hazel runs as an embedded script ("Setapp Installed") whenever a new app is added
-ls /Applications/Setapp | egrep '\.app$' > ~/.dotfiles/setapp-install.txt
+ls /Applications/Setapp | grep -E '\.app$' > ~/.dotfiles/setapp-install.txt
