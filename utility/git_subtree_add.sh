7ea6c6f698f96a4b30071db3b78af28c7045053e
Joseph Dickan <jdickan@ucla.edu>
2026-07-09 16:34:53 -0700

    Fix git-subtree scripts: cwd-dependence, .git suffix, hardcoded master
    - git_subtree_add.sh and git_subtree_setup_origins.sh both assumed
      git_subtrees.txt was reachable via a bare relative path, silently
      breaking unless run from inside utility/ - inconsistent with
      git_subtree_update_all.sh, which already guards against this.
    - The https:// URL branch of the name-parsing logic didn't strip a
      trailing .git the way the git@ branch did, producing names like
      "foo.git" instead of "foo". Replaced both branches with a single
      generic strip-then-basename approach.
    - git subtree add hardcoded "master" as the branch; now auto-detects
      the actual default branch via `git ls-remote --symref`.

--- utility/git_subtree_add.sh
+++ utility/git_subtree_add.sh
@@ -1,20 +1,29 @@
 #!/usr/bin/env zsh
 
+SRC_DIR=$(cd "$(dirname "$0")"; pwd)
+
 if [[ $# -lt 2 ]]; then
   echo "Usage: git_subtree_add.sh [URL] [LOCAL_PATH]"
   exit 1
 fi
 
-name=""
-if [[ ${1:0:14} = "git@github.com" ]]; then
-  name=$(echo $1 | sed -n "s/git@github.com:.*\/\(.*\).git/\1/p")
-elif [[ ${1:0:18} = "https://github.com" ]]; then
-  name=$(echo $1 | sed -n "s/https:\/\/github.com\/.*\/\(.*\)/\1/p")
+url="$1"
+if [[ ${url:0:14} = "git@github.com" ]] || [[ ${url:0:18} = "https://github.com" ]]; then
+  name="${url%.git}"
+  name="${name##*/}"
 else
   echo "ERROR: Couldn't parse URL. Do this manually."
   exit 1
 fi
 name=$name:l
 
-git subtree add --prefix=$2 --squash $1 master
-echo "$name	$1	$2" >> git_subtrees.txt
+default_branch=$(git ls-remote --symref $url HEAD | awk '/^ref:/ {sub("refs/heads/", "", $2); print $2}')
+if [[ -z $default_branch ]]; then
+  echo "ERROR: Couldn't detect default branch for $url. Do this manually."
+  exit 1
+fi
+
+cd "$SRC_DIR/.."
+git subtree add --prefix=$2 --squash $url $default_branch
+cd "$SRC_DIR"
+echo "$name	$url	$2" >> git_subtrees.txt
--- utility/git_subtree_setup_origins.sh
+++ utility/git_subtree_setup_origins.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env zsh
 
+SRC_DIR=$(cd "$(dirname "$0")"; pwd)
+
 while read data; do
   local name=$data[(w)1]
   local url=$data[(w)2]
 
   git remote add $name $url
-done <git_subtrees.txt
+done <"$SRC_DIR/git_subtrees.txt"
